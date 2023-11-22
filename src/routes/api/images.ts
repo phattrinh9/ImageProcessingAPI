26dc4a7dd93b4a0ac90d9878afa3d71668d9d1c5
phattrinh9 <phattrinh9@gmail.com>
2023-11-23 00:10:07 +0700

    clean code

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -49,21 +49,5 @@ images.get('/', async (req, res) => {
       res.status(500).send('Internal Server Error')
   }
 
-  // //use sharp for resize image
-  // sharp(imageUrl)
-  //   .resize(imageHeight, imageWidth) // Set the desired width and height
-  //   .toBuffer()
-  //   .then((data) => {
-  //     // Display the image or do further processing
-  //     // For example, if you are using Express.js, you can send the image as a response:
-  //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
-  //     res.end(data)
-  //   })
-  //   .catch((error) => {
-  //     // Handle any errors that occur during the process
-  //     console.error(error)
-  //     res.writeHead(404)
-  //     res.send('Can not find image')
-  //   })
 })
 export = images
